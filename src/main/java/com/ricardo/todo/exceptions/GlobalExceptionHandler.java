42038855c72601d9f8168d393caa2eefd1f9d3b2
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-12-29 22:51:32 -0300

    fix: adjust handleValidationErrors method

--- src/main/java/com/ricardo/todo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ricardo/todo/exceptions/GlobalExceptionHandler.java
@@ -11,21 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-// TODO: Update handleValidationErrors = not working
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
-        List<String> errors = ex.getBindingResult().getFieldErrors()
-                .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException exception) {
+        List<FieldError> errors = exception.getBindingResult().getFieldErrors();
         return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
-    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+    private Map<String, List<String>> getErrorsMap(List<FieldError> errors) {
         Map<String, List<String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
+        List<String> mappedErrors = errors.stream().map((f) -> f.getField() + " " + f.getDefaultMessage()).toList();
+        errorResponse.put("errors", mappedErrors);
         return errorResponse;
     }
 }
